f4486c1f39d41d62f894f974530ee73c033b0943
宁晓健 <664650322@qq.com>
2019-11-18 16:38:29 +0800

    增加了上传功能

--- youqu-service-oss/src/test/java/cn/zdxh/oss/YouquServiceOssApplicationTests.java
+++ youqu-service-oss/src/test/java/cn/zdxh/oss/YouquServiceOssApplicationTests.java
@@ -1,13 +1,19 @@
 package cn.zdxh.oss;
 
+import cn.zdxh.oss.service.QiniuFileService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class YouquServiceOssApplicationTests {
 
+    @Autowired
+    QiniuFileService service;
     @Test
     void contextLoads() {
+//        service.upload();
     }
+
 
 }
